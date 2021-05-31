576c68d987e0804ef70f4692be4b3c5177770a0b
the-unknown-source <67434542+the-unknown-source@users.noreply.github.com>
2021-06-01 00:21:22 +0530

    Update

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDEJu0QFKO4Qo2RvcAqpBnmvsftyyrYJPA",
-    authDomain: "twitter-react-clone-afe5b.firebaseapp.com",
-    projectId: "twitter-react-clone-afe5b",
-    storageBucket: "twitter-react-clone-afe5b.appspot.com",
-    messagingSenderId: "932886105406",
-    appId: "1:932886105406:web:28f0aea5738fd8359ef06f",
-    measurementId: "G-K5K2Q78Q6J"
+
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
